c78b582dc74abfa589048f9a75c014896c0b3d60
ManishReddyNaini945 <naini.manishreddy123@gmail.com>
2025-04-23 08:24:26 +0530

    created header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
+  import './App.css';
+  import { Route, Routes } from 'react-router-dom';
+  import Home from './pages/Home/Home';
+  import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-      </Routes>
-    </div>
-  );
-}
+  function App() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Routes>
+          
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
